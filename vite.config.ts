c1a9ab2625292cf2ee8f5075331e9b04f225b3e3
hsen <hsendeeb2@gmail.com>
2026-05-14 12:43:44 +0300

    rebuild docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/MyPortfolio/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss(),
